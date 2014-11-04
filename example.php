886c3606abc67eae3506032af9dcdf74f7ed3fc9
orenfxp <orenfxp2@gmail.com>
2014-11-05 00:52:16 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,26 +1,31 @@
 <?php
+if (($_post("first_value"])&&($_post["second_value"]))(
+    if ($_post["sign"]=="+")
+	    $result=$_post("first_value")+$_post["second_value"];
+	elseif ($_post("sign"]=="-")
+		    $result=$_post("first_value")-$_post["second_value"];
+	elseif ($_post("sign"]=="*")
+		    $result=$_post("first_value")*$_post["second_value"];
+	elseif ($_post("sign"]=="/")
+	   if ($_post["second_value"]!=0)
+		    $result=$_post("first_value")/$_post["second_value"];
+	   else
+	      $error="sorry// can't do it..";
 
-require_once 'src/Ftp.php';
-
-
-try {
-	$ftp = new Ftp;
-
-	// Opens an FTP connection to the specified host
-	$ftp->connect('ftp.ed.ac.uk');
-
-	// Login with username and password
-	$ftp->login('anonymous', 'example@example.com');
-
-	// Download file 'README' to local temporary file
-	$temp = tmpfile();
-	$ftp->fget($temp, 'README', Ftp::ASCII);
-
-	// echo file
-	echo '<pre>';
-	fseek($temp, 0);
-	fpassthru($temp);
-
-} catch (FtpException $e) {
-	echo 'Error: ', $e->getMessage();
-}
+	if ($error)
+	   echo "error: ".$error."<br><br>";
+	else ($error)
+	   echo "result: ".$result."<br><br>";
+)
+?>
+<form method="post">
+<input type="text" name="first_value">
+<select name="sign">
+<option value="+">+</option>
+<option value="-">-</option>
+<option value="*">*</option>
+<option value="/">/</option>
+</select>
+<input type="text" name="second_value">
+<input type="submit" value="show me the result">
+</form>
